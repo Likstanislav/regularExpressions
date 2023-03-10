75f636b175e55d1639a9692236399a30d95ad023
likstanislav <likstanislav@yandex.ru>
2023-03-10 19:25:00 +0700

    add matches

--- src/Main.java
+++ src/Main.java
@@ -3,11 +3,15 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
 //        System.out.println("Введите номер телефона:");
-//        Scanner scanner = new Scanner(System.in);
+        Scanner scanner = new Scanner(System.in);
 //        String number = scanner.nextLine();
 //        System.out.println(formatPhoneNumber(number));
 
-        System.out.println(splitText("I  know something   about   it"));
+//        System.out.println(splitText("I  know something   about   it"));
+
+        System.out.println("Введите номер авто:");
+        String number = scanner.nextLine();
+        System.out.println(isRightNumber(number));
 
     }
     //Замена одних фрагментов строк на другие
@@ -24,5 +28,9 @@ public class Main {
             result += words[i] + "\n";
         }
         return result;
+    }
+    public static Boolean isRightNumber(String number) {
+        String regex = "[АВЕКМНОРСТУХ][0-9]{3}[АВЕКМНОРСТУХ]{2}[0-9]{2,3}";
+            return number.matches(regex);
     }
 }
